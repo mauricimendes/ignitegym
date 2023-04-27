5511e466ad95d198a93e813c8dcc87e77ffadfd5
mauricimendes <mauricimendes.14@gmail.com>
2023-04-27 19:03:04 -0300

    first commit

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,78 @@
+
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import { useNavigation } from '@react-navigation/native'
+
+import LogoSvg from '@assets/logo.svg'
+import BackgroundImg from '@assets/background.png'
+
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
+
+export function SignUp() {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <VStack flex={1} px={10}>
+        <Image
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt='Pessoas treinando'
+          resizeMode='contain'
+          position='absolute'
+        />
+
+        <Center my={24}>
+
+          <LogoSvg />
+
+          <Text color='gray.100' fontSize='sm'>
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading
+            color='gray.100'
+            fontSize='xl'
+            mb={6}
+            fontFamily='heading'
+          >
+            Crie sua conta
+          </Heading>
+
+          <Input
+            placeholder='E-mail'
+            keyboardType='email-address'
+            autoCapitalize='none'
+          />
+
+          <Input
+            placeholder='Nome'
+            autoCapitalize='words'
+          />
+
+          <Input
+            placeholder='Senha'
+            secureTextEntry
+          />
+
+          <Button title='Criar e acessar' />
+
+        </Center>
+
+        <Button
+          title='Voltar para o login'
+          variant='outline'
+          mt={16}
+          mb={2}
+          onPress={handleGoBack}
+        />
+      </VStack>
+    </ScrollView>
+  )
+}
