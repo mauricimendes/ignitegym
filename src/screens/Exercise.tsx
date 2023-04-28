ebf7c447d6f1e722ae0ff21fd358d634e0fee2a6
mauricimendes <mauricimendes.14@gmail.com>
2023-04-27 21:43:13 -0300

    Refactoring styles

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Heading, Icon, Image, Text, VStack } from 'native-base'
+import { Box, HStack, Heading, Icon, Image, Text, VStack, ScrollView } from 'native-base'
 import { TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
@@ -20,6 +20,7 @@ export function Exercise() {
 
   return (
     <VStack flex={1}>
+
       <VStack px={8} bg='gray.600' pt={12}>
         <TouchableOpacity onPress={handleGoBack}>
           <Icon
@@ -44,39 +45,41 @@ export function Exercise() {
           </HStack>
         </HStack>
       </VStack>
+      <ScrollView showsVerticalScrollIndicator={false}>
 
-      <VStack p={8}>
-        <Image
-          w='full'
-          h={72}
-          source={{ uri: 'https://blog.meupersonalvirtual.com.br/wp-content/uploads/2019/04/284054-scheila-revisar-ate-0204-treino-na-academia-porque-fazer-exercicios-fisicos-na-academia-e-a-melhor-opcao.jpg' }}
-          alt='Nome do exercício'
-          mb={3}
-          resizeMode='cover'
-          rounded='lg'
-        />
+        <VStack p={8}>
+          <Image
+            w='full'
+            h={80}
+            source={{ uri: 'https://blog.meupersonalvirtual.com.br/wp-content/uploads/2019/04/284054-scheila-revisar-ate-0204-treino-na-academia-porque-fazer-exercicios-fisicos-na-academia-e-a-melhor-opcao.jpg' }}
+            alt='Nome do exercício'
+            mb={3}
+            resizeMode='cover'
+            rounded='lg'
+          />
 
-        <Box bg='gray.600' rounded='md' pb={4} px={4}>
-          <HStack alignItems='center' justifyContent='space-around' mb={6} mt={5}>
+          <Box bg='gray.600' rounded='md' pb={4} px={4}>
+            <HStack alignItems='center' justifyContent='space-around' mb={6} mt={5}>
 
-            <HStack>
-              <SeriesSvg />
-              <Text color='gray.200' ml='2'>
-                3 séries
-              </Text>
+              <HStack>
+                <SeriesSvg />
+                <Text color='gray.200' ml='2'>
+                  3 séries
+                </Text>
+              </HStack>
+
+              <HStack>
+                <RepetitionsSvg />
+                <Text color='gray.200' ml='2'>
+                  12 repetições
+                </Text>
+              </HStack>
             </HStack>
 
-            <HStack>
-              <RepetitionsSvg />
-              <Text color='gray.200' ml='2'>
-                12 repetições
-              </Text>
-            </HStack>
-          </HStack>
-
-          <Button title='Marcar como realizado' />
-        </Box>
-      </VStack>
+            <Button title='Marcar como realizado' />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   )
 }
